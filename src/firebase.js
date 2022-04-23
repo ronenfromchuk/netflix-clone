05b994931b95f3d5c222f75fc67e8e99ca144004
Ronen Fromchuk <90146636+ronenfromchuk@users.noreply.github.com>
2022-04-23 02:14:19 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpusdxJnCtmEi0d67B9fdjLx2zMszBjkg",
-    authDomain: "netflix-clone-b7a60.firebaseapp.com",
-    projectId: "netflix-clone-b7a60",
-    storageBucket: "netflix-clone-b7a60.appspot.com",
-    messagingSenderId: "70061680097",
-    appId: "1:70061680097:web:a0bf7671bb5ae1d9ffdea0"
+    apiKey: "***********************************",
+    authDomain: "***********************************",
+    projectId: "***********************************",
+    storageBucket: "***********************************",
+    messagingSenderId: "***********************************",
+    appId: "***********************************",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
